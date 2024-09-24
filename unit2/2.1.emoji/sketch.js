0727338ea94e85dba3e0bc7c438bcac90606650f
Salliison873 <130172645+Salliison873@users.noreply.github.com>
2024-09-23 23:03:10 -0400

    Update sketch.js

--- unit2/2.1.emoji/sketch.js
+++ unit2/2.1.emoji/sketch.js
@@ -7,10 +7,18 @@ function setup() {
 }
 
 function draw() {
+  //Body of the face
   background(220);
   fill("yellow");
   circle(200,200,250);
   stroke("Black");
+
+  //The mounth
+  fill("black");
+  strokeweight(1);
+  ellpse(100,50,10,50);
+  
+  
   
 
   rectMode(CENTER);

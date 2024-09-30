a34e1453c94caec8e3b74d1357224ae79569cb02
Salliison873 <130172645+Salliison873@users.noreply.github.com>
2024-09-30 19:38:37 -0400

    Update sketch.js

--- unit2/2.2.grid/sketch.js
+++ unit2/2.2.grid/sketch.js
@@ -3,7 +3,19 @@ function setup() {
   createCanvas(1200, 1200);
 
   // disable animation
-  noLoop();
+  //noLoop();
+}
+
+function Draw(){
+translate(100,100);
+for (let x = 0; x < 8; x++){
+   for (let y = 0; y < 8; y++){
+      push();
+      translate(x * 100, y * 100);
+      ellipse(0,0,100,100);
+      pop();
+     }
+  }
 }
 
 function draw() {

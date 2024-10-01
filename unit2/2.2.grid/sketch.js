56907cef8c7ba58502dba321fe6940dd17ac3b95
Salliison873 <130172645+Salliison873@users.noreply.github.com>
2024-10-01 11:54:24 -0400

    Update sketch.js

--- unit2/2.2.grid/sketch.js
+++ unit2/2.2.grid/sketch.js
@@ -4,27 +4,24 @@ function setup() {
   createCanvas(1200, 1200);
 
   // disable animation
-  noLoop();
 }
 
 
 function draw(){
-background(50, 55, 100);
+background('Black');
 translate(100,100);
 colorMode(HSB, 100);
-let RanColor = color(random(100), random(200), random(245))
 for (let x = 0; x < 8; x++){
    for (let y = 0; y < 8; y++){
       push();
+      let RanColor = color(random(100), random(200), random(245))
       translate(x * 100, y * 100);
       fill('white');
       rect(0,0,120,120);
       fill(RanColor);
       ellipse(50,50,100, 100);
-
       pop();
 
      }
   }
 }
-

730341fe67000417563cd55ac825d7fb83746b28
Salliison873 <130172645+Salliison873@users.noreply.github.com>
2024-10-03 12:04:32 -0400

    Update sketch.js

--- unit2/2.x.quilt-pattern/sketch.js
+++ unit2/2.x.quilt-pattern/sketch.js
@@ -1,7 +1,34 @@
 function setup() {
-  createCanvas(400, 400);
+  // create a canvas
+  // use https://p5js.org/reference/p5/random/ as a reference.
+  createCanvas(1200, 1200);
+  frameRate(3);
+
+  // disable animation
 }
 
-function draw() {
-  background(220);
+function draw(){
+background('black');
+translate(20,40);
+for (let x = 0; x < 8; x++){
+   for (let y = 0; y < 8; y++){
+      push();
+      let RanColor = color(random(100), random(200), random(300))
+      translate(x * 200, y * 300);
+      fill(RanColor);
+      ellipse(70,220,160, 160);
+     pop();
+   }
+
+for(let h = 0; h < 8; h++){
+    for(let i = 0; i < 8; i++){
+        push();
+        let RanColor2 = color(random(100), random(200), random(300))
+        translate(i * 200, h * 300);
+        fill(RanColor2);
+        rect(0,0,140,140);
+        pop();
+        }
+     }  
+   }
 }

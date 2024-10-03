fad55c788a79c324c69134b15ed8c2d62cce7762
Salliison873 <130172645+Salliison873@users.noreply.github.com>
2024-10-03 13:42:04 -0400

    Update sketch.js

--- unit2/2.2.grid/sketch.js
+++ unit2/2.2.grid/sketch.js
@@ -2,33 +2,25 @@ function setup() {
   // create a canvas
   // use https://p5js.org/reference/p5/random/ as a reference.
   createCanvas(1200, 1200);
-  frameRate(3); // how fast it goes through the loop each time
 
   // disable animation
+  noLoop();
 }
 
 function draw(){
-background('black'); // The color of the background
-translate(20,40);
+background('Black');
+translate(100,100);
+colorMode(HSB, 100);
 for (let x = 0; x < 8; x++){
    for (let y = 0; y < 8; y++){
       push();
-      let RanColor = color(random(100), random(200), random(300)) // Each time it goes through the loop it chooses a different color for the cirlces
-      translate(x * 200, y * 300);
+      let RanColor = color(random(100), random(200), random(245))
+      translate(x * 100, y * 100);
+      fill('white');
+      rect(0,0,120,120);
       fill(RanColor);
-      ellipse(70,220,160, 160);
-     pop();
-   }
-
-for(let h = 0; h < 8; h++){
-    for(let i = 0; i < 8; i++){
-        push();
-        let RanColor2 = color(random(100), random(200), random(300)) // Each time it goes through the loop it chooses a different color for the rect
-        translate(i * 200, h * 300);
-        fill(RanColor2);
-        rect(0,0,140,140);
-        pop();
-        }
-     }  
-   }
+      ellipse(50,50,100, 100);
+      pop();
+     }
+  }
 }

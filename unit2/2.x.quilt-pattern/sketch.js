e98b54276f4b1216406bf248fa189e09fc78494f
Salliison873 <130172645+Salliison873@users.noreply.github.com>
2024-10-03 12:09:45 -0400

    Update sketch.js

--- unit2/2.x.quilt-pattern/sketch.js
+++ unit2/2.x.quilt-pattern/sketch.js
@@ -2,18 +2,18 @@ function setup() {
   // create a canvas
   // use https://p5js.org/reference/p5/random/ as a reference.
   createCanvas(1200, 1200);
-  frameRate(3);
+  frameRate(3); // how fast it goes through the loop each time
 
   // disable animation
 }
 
 function draw(){
-background('black');
+background('black'); // The color of the background
 translate(20,40);
 for (let x = 0; x < 8; x++){
    for (let y = 0; y < 8; y++){
       push();
-      let RanColor = color(random(100), random(200), random(300))
+      let RanColor = color(random(100), random(200), random(300)) // Each time it goes through the loop it chooses a different color for the cirlces
       translate(x * 200, y * 300);
       fill(RanColor);
       ellipse(70,220,160, 160);
@@ -23,7 +23,7 @@ for (let x = 0; x < 8; x++){
 for(let h = 0; h < 8; h++){
     for(let i = 0; i < 8; i++){
         push();
-        let RanColor2 = color(random(100), random(200), random(300))
+        let RanColor2 = color(random(100), random(200), random(300)) // Each time it goes through the loop it chooses a different color for the rect
         translate(i * 200, h * 300);
         fill(RanColor2);
         rect(0,0,140,140);

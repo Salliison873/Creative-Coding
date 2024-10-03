fd3749f261fc562a0c7bc454f0e4e2f759621bd4
Salliison873 <130172645+Salliison873@users.noreply.github.com>
2024-10-03 13:11:45 -0400

    Add files via upload

---
+++ unit2/2.x.pixel-sort/sketch.js
@@ -0,0 +1,45 @@
+function setup() {
+  // create a canvas
+  createCanvas(400, 400);
+
+  // disable animation
+  noLoop();
+}
+
+function draw() {
+  //Body of the face
+  background(220);
+  fill("yellow");
+  stroke("black");
+  circle(200,200,250);
+
+  //The mounth
+  fill("#000000");
+  strokeWeight(20);
+  line(160,200,200,200);
+
+  //Right eye
+  fill("#000000");
+  strokeWeight(1);
+  rect(225,140,60,20);
+
+  //Left eye
+  fill("#000000");
+  strokeWeight(1);
+  rect(125,140,60,20);
+
+
+  
+  
+
+  
+  
+  
+  
+  
+  
+  
+
+  rectMode(CENTER);
+ 
+}

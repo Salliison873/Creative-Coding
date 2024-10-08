0931f2be182be541ed6899340498426856a54b68
Salliison873 <130172645+Salliison873@users.noreply.github.com>
2024-10-08 11:48:59 -0400

    Update sketch.js

--- unit2/2.3.emulation/sketch.js
+++ unit2/2.3.emulation/sketch.js
@@ -1,20 +1,21 @@
 function setup() {
   // create a canvas
   // use https://p5js.org/reference/p5/random/ as a reference.
-  createCanvas(1200, 1200);
+  createCanvas(700, 800);
 
   // disable animation
   noLoop();
 }
+
 function draw(){
 background('white');
-translate(100,100);
+translate(6,105);
 colorMode(HSB, 100);
 for (let x = 0; x < 30; x+=1){
    for (let y = 0; y < 30; y+=1){
      push();
      translate(x * 100, y * 100);
-     stroke('#00BCD4'); // the fill outline of the color
+     stroke('#b6d2cd'); // the fill outline of the color
      strokeWeight(5); // the thickess of the outline of the squares.
      rect(0,0,80,80); // the squares
      pop();
@@ -22,11 +23,15 @@ for (let x = 0; x < 30; x+=1){
 }
 for (let x = 5; x < 15; x++){
    for (let y = 5; y < 15; y++){
+     let randomX = random(80,530);
+     let randomY = random(50,530);
+     let color = ["#b4453c","#92608e","#b48a5f"];
+     let chosenColor = random(color);
      push();
-     let RanColor = color(random(100), random(256), random(300));
-     translate(x * random(100), y * random(200));
-     stroke(RanColor); // the fill outline of the color
+     translate(randomX, randomY);
+     stroke(chosenColor)
      strokeWeight(5); // the thickess of the outline of the squares.
+     noFill();
      rect(0,0,80,80); // the squares
      pop();
    }

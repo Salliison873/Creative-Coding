fd993ac54940abe0c3e65ed4df8cfc0a31958abc
Salliison873 <130172645+Salliison873@users.noreply.github.com>
2024-10-25 14:24:11 -0400

    Update sketch.js

--- unit2/landscape/sketch.js
+++ unit2/landscape/sketch.js
@@ -1,67 +1,24 @@
-function preload(){
-  //Use p5.j as a reference.
-  // load the image from a file
-	img = loadImage(unit2/landscape/IMG_5559.jpg);
-}
-
 function setup() {
-
-  // create the canvas
-  createCanvas(800, 800);
-  
-}
-
-function draw() {
-  background(220);
-	let from = color(0, 120, 100);
-  let to = color(72, 61, 50);
-
-  //intermediate colors.
-  let interA = lerpColor(from, to, 0.01);//blends it out so it looks all one color
-  let interB = lerpColor(from, to, 0.01);
-
-  //left rectangle.
+  createCanvas(400, 400);
   noStroke();
-  fill(from);
-  rect(1, 100, 400, 100);
-
-  // left-center rectangle.
-  fill(interA);
-  rect(1, 0, 400, 100);
-
-  //right-center rectangle.
-  fill(interB);
-  rect(1, 200, 400, 100);
-
-  // right rectangle.
-  fill(to);
-  rect(1, 300, 400, 100);
-  for (let x = 0; x <= width; x++) { //loop allows to make the mountain range.
-    let y = noise(x * 0.005) * height; // the height of the mountain
-    circle(x,y,5); //creates the image
-    stroke('#2D6330');  // color of the image
+  noLoop();
 }
-  for (let x = 0; x <= width; x++) { //loop allows to make the mountain range.
-    let y = noise(x * 0.005 + 6) * height; // the height of the mountain
-    circle(x,y += 5 ,5); //creates the image
-    stroke('#2D6330'); // color of the image
-  }
-  for (let x = 0; x <= width; x++) { //loop allows to make the mountain range.
-    let y = noise(x * 0.005 + 3) * height; // the height of the mountain
-    circle(x,y += 5 ,5); //creates the image
-    stroke('#2D6330'); // color of the image
-  }
-  for (let x = 0; x <= width; x++) { //loop allows to make the mountain range.
-    let y = noise(x * 0.005 + 2) * height; // the height of the mountain
-    circle(x,y += 5 ,5); //creates the image
-    stroke('#2D6330'); // color of the image 
-  }
-  for (let x = 0; x <= width; x++) { //loop allows to make the mountain range.
-    let y = noise(x * 0.005 + 10) * height; // the height of the mountain
-    circle(x,y += 5 ,5); //creates the image
-    stroke('#2D6330'); // color of the image 
-  }
-  // render the image
-  image(img,10,10,100,100);
-}
+function draw(){
+  background('white');
+  fill('#03A9F4')
+  rect(0,0,400,400);
+  let from = color('#795548'); // start here 
+  let to = color('rgb(20,204,20)'); // end here
+  
+  for (let x = 0; x < 400; x++) {
+    let y = floor(noise(x * 0.005) * 300); //Create the outline for the different mountain view 
+    fill('rgb(20,204,20)');
+    circle(x, y, 4);
+    for (let m = y; m < 400; m++) { // fills the color the different line. 
+      let fill_color = lerpColor(to, from, m / 400);
+      fill(fill_color);
+      rect(x,m,1,100);
 
+    }
+  }
+}

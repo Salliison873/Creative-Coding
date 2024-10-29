3b27f784ef55087d26d385fbfb883dbb7669aa14
Salliison873 <130172645+Salliison873@users.noreply.github.com>
2024-10-28 22:25:04 -0400

    Update sketch.js

--- unit3/3.3.ripples/sketch.js
+++ unit3/3.3.ripples/sketch.js
@@ -1,7 +1,45 @@
+//use your video as a reference 
+let ripple_x = 0;
+let ripple_y = 0;
+let ripple_d = 50;
+
+
+
 function setup() {
   createCanvas(800, 800);
 }
 
+class Rippler {
+  constructor(x,y){
+    this.x = x,
+    this.y = y,
+    this.d = 0
+  }
+  draw(){
+    this.d += 1;
+    circle(this.x, this.y,this.d);
+  }
+}
+let ripplers = [];
+
 function draw() {
-  background(220);
+    background('#03A9F4'); //the color of the background
+    stroke('blue'); //the color of the ripplers
+    strokeWeight(5);
+    noFill();
+    for(let i = 0; i < ripplers.length; i++){
+      ripplers[i].draw(); 
+    }
+    
+ 
+
+}
+
+function mousePressed(){
+  
+  ripple_x = mouseX;
+  ripple_y = mouseY;
+  ripple_d = 0; // allows the click to create the rippler effect. 
+  
+  ripplers.push( new Rippler(mouseX, mouseY));
 }

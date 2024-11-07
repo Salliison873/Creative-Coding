411366f9871c79a2b5585e0f7892265915a6a66c
Salliison873 <130172645+Salliison873@users.noreply.github.com>
2024-11-06 22:35:01 -0500

    Update sketch.js

--- unit3/time/sketch.js
+++ unit3/time/sketch.js
@@ -1,4 +1,32 @@
- // Draw the clock background
+// Use P5.j reference as a reference 
+
+
+// Declare variables for shape radius
+let secondsRadius;
+let minutesRadius;
+let hoursRadius;
+let clockDiameter;
+
+function setup() {
+  createCanvas(400, 500);
+  stroke(255);
+  angleMode(DEGREES);
+
+  // Set radius for each shape based on canvas dimensions
+  let radius = min(width, height) / 2;
+  secondsRadius = radius * 0.71;
+  minutesRadius = radius * 0.6;
+  hoursRadius = radius * 0.5;
+  clockDiameter = radius * 1.7;
+}
+
+function draw() {
+  background("#FFF7AD");
+
+  // Move origin to center of canvas
+  translate(width / 2, height / 2-40);
+
+  // Draw the clock background
   noStroke();
   fill('#C256BA');
   ellipse(0, 0, clockDiameter + 25, clockDiameter + 25);
@@ -39,11 +67,13 @@ background(220);
 
   // Tick markers around perimeter of clock
   push();
-  strokeWeight(2);
+  strokeWeight(10);
   for (let ticks = 0; ticks < 60; ticks += 1) { 
+    point(0, -secondsRadius - 20 );
     point(0, -secondsRadius);
+    point(0, -secondsRadius /8);
     rotate(6);
   }
   pop();
-  
+ 
 }

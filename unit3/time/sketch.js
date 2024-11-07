4d502395b4a030d3a56a93066c3a7e07cda58de0
Salliison873 <130172645+Salliison873@users.noreply.github.com>
2024-11-07 14:51:59 -0500

    Update sketch.js

--- unit3/time/sketch.js
+++ unit3/time/sketch.js
@@ -29,7 +29,7 @@ function draw() {
   background("#FFF7AD");
 
   // Move origin to center of canvas
-  translate(width / 2, height / 2-40);
+  translate(width / 2, height / 2-20);
 
   // Draw the clock background
   noStroke();

82ca7c5f93688bb6f2c76d07a5d924506e86c18e
Salliison873 <130172645+Salliison873@users.noreply.github.com>
2024-11-07 13:52:47 -0500

    Update sketch.js

--- unit3/time/sketch.js
+++ unit3/time/sketch.js
@@ -37,7 +37,7 @@ function draw() {
   ellipse(0, 0, clockDiameter + 25, clockDiameter + 25);
   fill('#ffa9f9');
   ellipse(0, 0, clockDiameter, clockDiameter);
-  image(img, -100, -200);
+  image(img, -100, -150);
 
 function draw() {
 background(220);

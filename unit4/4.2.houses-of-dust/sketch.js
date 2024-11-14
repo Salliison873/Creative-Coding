831bc909ce17bd36c4201c0a4835b8cfbfb68d8b
Salliison873 <130172645+Salliison873@users.noreply.github.com>
2024-11-14 12:10:52 -0500

    Update sketch.js

--- unit4/4.2.houses-of-dust/sketch.js
+++ unit4/4.2.houses-of-dust/sketch.js
@@ -1,7 +1,35 @@
+let quartrain = '':
+let gram = tracery.createGrammer({
+ {
+ "material" : 
+  ["DUST", 
+   "BRICK", 
+   "PAPER", 
+   "LEAVES", 
+   "ROOTS", 
+   "DISCARDED CLOTHING", 
+   "WEEDS", 
+   "WOOD",
+   "TIN", 
+   "SAND",
+   "GLASS",
+   "STONE", 
+   "STRAW", 
+   "PLASTIC",
+   "MUD", 
+   "BROKEN DISHES",
+   "PLASTIC"],
+  "origin": "A HOUSE OF #material#
+})
+quartrain = grammer.flattern("#origin#"); 
 function setup() {
   createCanvas(800, 800);
+  noloop();
+  
 }
 
 function draw() {
   background(220);
+  textSize(48);
+  text("quartrain50,50);
 }

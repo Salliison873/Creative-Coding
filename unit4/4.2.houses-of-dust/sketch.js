a1db8c7652d22f288b59527e3285c4189f86f45e
Salliison873 <130172645+Salliison873@users.noreply.github.com>
2024-11-14 23:55:53 -0500

    Update sketch.js

--- unit4/4.2.houses-of-dust/sketch.js
+++ unit4/4.2.houses-of-dust/sketch.js
@@ -1,4 +1,7 @@
 let quatrain = '';
+var places = '';
+var lights = 'natural light;candles;all available lighting;electricity'.split(';');
+var inhabitants = 'people who eat a great deal;lovers;friends and enemies;collectors of all types;women wearing all colors;all races of men wearing predominantly red clothing;little boys;children and old people;horses and birds;various birds and fishes;american indians;french and german speaking people;people who sleep almost all the time'.split(';');
 
 let grammar = tracery.createGrammar({
   "material": [
@@ -16,6 +19,24 @@ let grammar = tracery.createGrammar({
   ],
   "origin": "A HOUSE OF #material#"
 });
+let words = tracery.createGrammar({
+	"phrase": [
+		"on open ground",
+		"by a river"
+		"among high mountains",
+		"in a deserted airport",
+		"in dense woods",
+		"on an island",
+		"in a hot climate",
+		"in a desert",
+		"on the sea",
+		"in a deserted factory",
+		"underwater",
+		"in southern France",
+		inside a mountain;in an overpopulated area;in japan;in a place with both heavy rain and bright sun;by an abandoned lake;in michigan;by the sea;in a cold, windy climate;among small hills'.split(';');
+
+
+})
 
 quatrain = grammar.flatten("#origin#");
 

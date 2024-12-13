bb4d0be5bbdf45def0df1a5f243fda595648aa3c
Salliison873 <130172645+Salliison873@users.noreply.github.com>
2024-12-12 21:27:34 -0500

    Update sketch.js

--- unit5/final/sketch.js
+++ unit5/final/sketch.js
@@ -11,10 +11,10 @@ function draw(){
 background('Black');
 translate(100,100);
 colorMode(HSB, 100);
-for (let x = 0; x < 5; x++){
-   for (let y = 0; y < 5; y++){
+for (let x = 0; x < 4; x++){
+   for (let y = 0; y < 4; y++){
       push();
-      translate(x * 200, y * 200);
+      translate(x * 150, y * 150);
       fill('white');
       rect(0,0,120,120);
       pop();

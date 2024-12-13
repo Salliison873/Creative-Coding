b4fe4ff084090a4b18ff125cb570f99455fb9521
Salliison873 <130172645+Salliison873@users.noreply.github.com>
2024-12-12 21:23:34 -0500

    Update sketch.js

--- unit5/final/sketch.js
+++ unit5/final/sketch.js
@@ -2,33 +2,22 @@ function setup() {
   // create a canvas
   // use https://p5js.org/reference/p5/random/ as a reference.
   createCanvas(1200, 1200);
-  frameRate(3);
 
   // disable animation
+  noLoop();
 }
 
 function draw(){
-background('black');
-translate(20,40);
+background('Black');
+translate(100,100);
+colorMode(HSB, 100);
 for (let x = 0; x < 8; x++){
    for (let y = 0; y < 8; y++){
       push();
-      let RanColor = color(random(100), random(200), random(300))
-      translate(x * 200, y * 300);
-      fill(RanColor);
-      ellipse(70,220,160, 160);
-     pop();
-   }
-
-for(let h = 0; h < 8; h++){
-    for(let i = 0; i < 8; i++){
-        push();
-        let RanColor2 = color(random(100), random(200), random(300))
-        translate(i * 200, h * 300);
-        fill(RanColor2);
-        rect(0,0,140,140);
-        pop();
-        }
-     }  
-   }
+      translate(x * 100, y * 100);
+      fill('white');
+      rect(0,0,120,120);
+      pop();
+     }
+  }
 }

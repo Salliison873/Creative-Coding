f7533b5abeafce24a4dd2ac7174280494658abe2
Salliison873 <130172645+Salliison873@users.noreply.github.com>
2024-12-13 23:22:50 -0500

    Update sketch.js

--- unit2/2.1.emoji/sketch.js
+++ unit2/2.1.emoji/sketch.js
@@ -1,45 +1,52 @@
 function setup() {
-  // create a canvas
-  createCanvas(400, 400);
-
-  // disable animation
+  // Create a canvas
+  createCanvas(800, 400);
+  // Disable animation
   noLoop();
 }
 
 function draw() {
-  //Body of the face
+  // Set the background color
   background(220);
-  fill("yellow");
-  stroke("black");
-  circle(200,200,250);
   
-  //left Eye
+  // Draw the first emoji face
+  drawFace(150, 200, "yellow", "happy");
+  
+  // Draw the second emoji face
+  drawFace(400, 200, "blue", "sad");
+  
+  // Draw the third emoji face
+  drawFace(650, 200, "green", "surprised");
+}
+
+// Function to draw a face at a specified location with a specified color and expression
+function drawFace(x, y, faceColor, expression) {
+  // Body of the face
+  fill(faceColor);
+  stroke("black");
+  circle(x, y, 150);
+
+  // Left Eye
   fill("white");
   stroke("black");
-  circle(150,150,90);
+  circle(x - 30, y - 30, 40);
   fill("black");
-  stroke("black");
-  circle(150,132,55);
-  
+  circle(x - 30, y - 30, 20);
+
   // Right Eye
   fill("white");
   stroke("black");
-  circle(250,150,90);
+  circle(x + 30, y - 30, 40);
   fill("black");
-  stroke("black");
-  circle(250,132,55);
-  
+  circle(x + 30, y - 30, 20);
+
   // Mouth
   fill("black");
-  strokeWeight(25);
-  line(120,250,275,250);
-  rectMode(CENTER);
- 
+  if (expression === "happy") {
+    arc(x, y + 10, 70, 70, 0, PI); // Happy mouth
+  } else if (expression === "sad") {
+    arc(x, y + 40, 70, 70, PI, 0); // Sad mouth
+  } else if (expression === "surprised") {
+    ellipse(x, y + 20, 70, 30); // Surprised mouth
+  }
 }
-
-  
-  
-  
-  
-  
-  

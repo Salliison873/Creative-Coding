40fd453871497dd13e44a21c811c13135727f17c
Salliison873 <130172645+Salliison873@users.noreply.github.com>
2024-12-13 21:27:06 -0500

    Update and rename Sketch2.js to sketch2.js

--- unit2/2.2.grid/sketch2.js
+++ unit2/2.2.grid/sketch2.js
@@ -13,17 +13,17 @@ translate(20,40);
 for (let x = 0; x < 8; x++){
    for (let y = 0; y < 8; y++){
       push();
-      let RanColor = color(random(100), random(200), random(300))
+      let RanColor = color(random(300), random(100), random(400))
       translate(x * 200, y * 300);
       fill(RanColor);
-      ellipse(70,220,160, 160);
+      triangle(70,220,160, 160);
      pop();
    }
 
 for(let h = 0; h < 8; h++){
     for(let i = 0; i < 8; i++){
         push();
-        let RanColor2 = color(random(100), random(200), random(300))
+        let RanColor2 = color(random(400), random(600), random(500))
         translate(i * 200, h * 300);
         fill(RanColor2);
         rect(0,0,140,140);
